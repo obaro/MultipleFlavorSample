bc418ee659a66a300a34eb5a40f75603e97118ea
obaro <Obaro.ogbo@gmail.com>
2016-07-31 14:00:42 +0100

    Initial commit

---
+++ app/src/blueberry/java/com/sample/foo/blueberry/BlueActivity.java
@@ -0,0 +1,15 @@
+package com.sample.foo.blueberry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sample.foo.multipleflavorsample.R;
+
+public class BlueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue);
+    }
+}
